951c0897c9c58367461b117aece023667ee823e7
Jean personal <jeanschoen@hotmail.com>
2024-03-08 12:17:17 +0530

    added Dialogue manager with a frigidAir Pop-Up

---
+++ Code/Scripts/Managers/DialogueManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI; // Make sure to include this for UI components
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Animator FridgePopUpAnimator;
+    [SerializeField] TextMeshProUGUI dialogueText; // Assign this in the inspector
+    private bool isDialogueActive = false;
+    private string[] dialogues; // Array to hold your dialogues
+    private int currentDialogueIndex = 0;
+
+    void Start()
+    {
+        // Initialize your dialogues here, or set them from somewhere else
+        dialogues = new string[] {
+            "[Figidair] \n Graaar, me want oiiiil, give me that sweet soothing liquid !  \n\n (Click anywhere to continue)",
+            "Dialogue 2 text here...",
+            "Dialogue 3 text here..."
+            // Add as many dialogues as you need
+        };
+    }
+
+    public void OnDialogueAreaClicked()
+    {
+        if (!isDialogueActive)
+        {
+            TriggerPopUp();
+        }
+        else
+        {
+            if (currentDialogueIndex < dialogues.Length - 1)
+            {
+                // Go to next dialogue
+                currentDialogueIndex++;
+                UpdateDialogueText();
+            }
+            else
+            {
+                // Last dialogue; hide the dialogue box
+                Debug.Log("trigger pop down");
+                TriggerPopDown();
+            }
+        }
+    }
+
+    public void TriggerPopUp()
+    {
+        isDialogueActive = true;
+        FridgePopUpAnimator.gameObject.SetActive(true);
+        FridgePopUpAnimator.SetTrigger("PopUp");
+        currentDialogueIndex = 0; // Reset dialogue index
+        UpdateDialogueText();
+    }
+
+    private void TriggerPopDown()
+    {
+        FridgePopUpAnimator.SetTrigger("PopDown"); // Ensure you have a PopDown animation
+        isDialogueActive = false;
+        // Optionally reset currentDialogueIndex or wait until next TriggerPopUp
+    }
+
+    private void UpdateDialogueText()
+    {
+        if (dialogueText != null && dialogues.Length > 0)
+        {
+            dialogueText.text = dialogues[currentDialogueIndex];
+        }
+    }
+}
