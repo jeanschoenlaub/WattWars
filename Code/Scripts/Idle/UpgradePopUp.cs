995fd471f27e6664dd63134eefe8224e3b1d5d60
Jean personal <jeanschoen@hotmail.com>
2024-04-15 15:57:33 +0545

    made backbone of Idle Game UI

---
+++ Code/Scripts/Idle/UpgradePopUp.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradePopUp : MonoBehaviour
+{
+    [Header("--------- References ---------")]
+    [SerializeField] private GameObject popUpGameObject;
+    private bool isUpgradeOpen = false;
+
+    private void Start() {
+        popUpGameObject.SetActive(false);
+    }
+
+    public void TogglePopUp(){
+        isUpgradeOpen = !isUpgradeOpen;
+        
+        if (isUpgradeOpen)
+        {
+            popUpGameObject.SetActive(true);
+        }
+        else
+        {
+            popUpGameObject.SetActive(false);
+        }
+    }
+}
